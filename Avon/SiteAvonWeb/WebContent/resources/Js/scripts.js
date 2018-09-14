8ce7c631371c6dc3fb41cd772281d1ca0c5bd08a
Daniil <danukrus@yandex.ru>
2018-09-14 15:30:23 +0300

    upd Avon

--- Avon/SiteAvonWeb/WebContent/resources/Js/scripts.js
+++ Avon/SiteAvonWeb/WebContent/resources/Js/scripts.js
@@ -1,29 +1,21 @@
 var catalogs = [];
 
-jQuery(document).ready(function() {
+$(document).ready(function() {
 	
 	$('.mainLogo, .altLogo').click(function() {
 		$(location).attr('href','index.html');
 	});
 	
-	$('#2button').click(function() {
-		$(location).attr('href','Catalog.html');
+	$('.beClient').click(function (){
+		$(location).attr('href','RegistrationClient.html');
 	});
 	
-	$('#3button').click(function() {
-		$(location).attr('href','Catalog.html');
+	$('.beRepr').click(function (){
+		$(location).attr('href','Registration.html');
 	});
 	
-	$('#4button').click(function() {
-		$(location).attr('href','Catalog.html');
-	});
-	
-	$('#5button').click(function() {
-		$(location).attr('href','Shares.html');
-	});
-	
-	$('.goToForm').click(function() {
-		$(location).attr('href','https://myoffice.avon.ru/SPF.html?beautybusiness');
+	$('.beCoord').click(function (){
+		$(location).attr('href','RegistrationCoord.html');
 	});
 
 	
@@ -38,6 +30,9 @@ jQuery(document).ready(function() {
 			
 			$('.mainLogo').hide();
 			$('.altLogo').show();
+			
+			$('.agreement').hide();
+			$('.agreementAlt').show();
 		}
 		else{
 			$('.largeMenu').show();
@@ -48,6 +43,9 @@ jQuery(document).ready(function() {
 			
 			$('.mainLogo').show();
 			$('.altLogo').hide();
+			
+			$('.agreement').show();
+			$('.agreementAlt').hide();
 		}
 	}
 	
@@ -70,19 +68,13 @@ jQuery(document).ready(function() {
 		// Генерируем даты каталогов на 5 лет
 		for (var i = 0; i < 86; i++){
 			catalogs[i] = init_day.getTime();
-//			console.log(init_day);
 			init_day.setDate(init_day.getDate() + 21);
 		}
 		
-		
-//		console.log("Текущий день: " + current_date + " \n              " + new Date(catalogs[0]) + " \n              " + new Date(catalogs[1]));
-//		console.log(catalogs[0] + " " + catalogs[1] + " " + catalogs[2]);
-		
-//		console.log("Compare: " + (catalogs[1].valueOf() < catalogs[2].valueOf()));
 		var i = 1;
 		while(true){
 			if (current_date.getTime() > catalogs[i-1] && current_date.getTime() < catalogs[i]){
-//				console.log("True")
+
 				$('#textCat1').html('<b>Avon ' + (11 + i) + ' ' + new Date(catalogs[i-1]).getFullYear() +'</b>');
 				$('#textCat2').html('<b>Avon ' + (11 + i - 1) + ' ' + new Date(catalogs[i-1]).getFullYear() +'</b>');
 				$('#imgCat1').attr('src', 'resources/catalogs/' + (11 + i) + '.jpg');
@@ -92,29 +84,20 @@ jQuery(document).ready(function() {
 				var day = new Date(catalogs[i-1]).getDate() <= 9 ? ('0' + new Date(catalogs[i-1]).getDate()) : new Date(catalogs[i-1]).getDate(); 
 				var month = new Date(catalogs[i-1]).getMonth()+1 <= 9 ? ('0' + (new Date(catalogs[i-1]).getMonth()+1)) : new Date(catalogs[i-1]).getMonth()+1;
 				
-				var next_day = new Date(catalogs[i]).getDate()-1 <= 9 ? ('0' + new Date(catalogs[i]).getDate())-1 : new Date(catalogs[i]).getDate()-1; 
+				var next_day = new Date(catalogs[i]).getDate()-1 <= 9 ? ((new Date(catalogs[i]).getDate()-1 == 0 ? 31 : '0' + new Date(catalogs[i]).getDate()-1)) : '0' + new Date(catalogs[i]).getDate()-1;				
 				var next_month = new Date(catalogs[i]).getMonth()+1 <= 9 ? ('0' + (new Date(catalogs[i]).getMonth()+1)) : new Date(catalogs[i]).getMonth()+1;
 				
 				$('#catalogDate').html((11+i-1) + '. ' + new Date(catalogs[i]).getFullYear() + ' (с ' + day + '.' + month + ' по ' + next_day + '.' + next_month + ')');
 				return;
 			}
 			else
-//				console.log("False")
 				
 			i++;
 		}
-//		while(true){
-//			
-//			if (current_date.getTime() < catalogs[i].getTime() && current_date.getTime() > catalogs[i-1].getTime()){
-//				$('.catalog p').html('<b>Avon ' + (11 + i + 1)  + '</b><');
-//				return;
-//			}
-//			i++;
-//		}
 	}
 	
 	$(function (){
-//		getCatalogs();
+		getCatalogs();
 		currentCatalog();
 		checkWidth($(window).width());
 	});
@@ -126,6 +109,14 @@ jQuery(document).ready(function() {
 	// Переход на страницу с каталогами
 	$('#CatalogsBut').click(function(){
 		$(location).attr('href','Catalogs.html');
+	});
+	
+	// Копирование адреса
+	$('#checkAdress').click(function (){
+		if ($('#checkAdress').prop('checked') == true)
+			$('#propAdress').val($('#factAdress').val());
+		else
+			$('#propAdress').val('');
 	});
 	
 });
